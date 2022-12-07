f01de352ba5716535c121c1ce91f4ceee8d8d122
YogitaDevi <yogita.gry@gmail.com>
2022-12-07 14:08:30 +0530

    LineComparison

---
+++ LineComparison/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using LineComparison;
+
+Console.WriteLine("Welocome to Line Comparison Program");
+
+Console.WriteLine("Calling CompareTwoLines");
+CompareTwoLine twoLines = new CompareTwoLine();
+twoLines.Compare();
+
+Console.WriteLine("Calling CheckEquality");
+CheckEquality check = new CheckEquality();
+check.Equality();
+
+Console.WriteLine("Caling LengthOfLine");
+LenghtOfLine ofLine = new LenghtOfLine();
+ofLine.Length();
+
